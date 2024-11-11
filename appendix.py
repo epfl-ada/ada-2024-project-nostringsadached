c21cf5d1c9c0c5da58e50fcc42d5eb10c33586b4
laurine-sg <laurine.song@epfl.ch>
2024-11-11 04:07:46 +0100

    i gotta sleep

--- appendix.py
+++ appendix.py
@@ -25,3 +25,7 @@ def outliers_bounds(data, low_factor, up_factor):
     lower_bound = Q1 - low_factor * IQR
     upper_bound = Q3 + up_factor * IQR
     return lower_bound, upper_bound
+
+def replace_in_columns(data, column, old, new):
+    data[column] = data[column].replace(old, new)
+    return data
